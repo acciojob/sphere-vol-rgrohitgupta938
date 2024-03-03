1baac304123d6f30815dee88c99a36b7093f7e2b
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-03-03 15:30:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
+function volume_sphere(e) {
+  e.preventDefault();
+  const radiusInput = parseFloat(document.getElementById("radius").value);
+  console.log(radiusInput);
+  const vol = (4 / 3) * Math.PI * radiusInput ** 3;
+  document.getElementById("volume").value = vol.toFixed(2);
+  return false;
+}
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = function() {
+  document.getElementById("MyForm").onsubmit = volume_sphere; // Bind the function to the form submit event
+};
